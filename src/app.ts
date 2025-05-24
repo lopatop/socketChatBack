9a26755d8cc72208f09296d10889196c85523b4d
lopatop <pavellopato327@gmail.com>
2025-05-24 22:02:38 +0200

    fix: add CORS config for socket.io

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,12 @@ import type { Socket } from "socket.io";
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"]
+    }
+});
 
 app.get('/', (req, res) => {
     res.send("Hello it's WS server ");
